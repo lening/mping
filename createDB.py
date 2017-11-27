58770a3a6f88406b6e3a4904239dca08853ff009
root <root@localhost.localdomain>
2017-11-27 15:39:59 +0800

    fix some bug

--- createDB.py
+++ createDB.py
@@ -2,7 +2,7 @@ import sqlite3
 databasePath='./mping.db'
 conn=sqlite3.connect(databasePath)
 cu=conn.cursor()
-sql='CREATE TABLE HOST_LATENCY (ID INTEGER PRIMARY KEY AUTOINCREMENT,HOST TEXT NOT NULL,LATENCY INT NOT NULL)'
+sql='CREATE TABLE HOST_LATENCY (ID INTEGER PRIMARY KEY AUTOINCREMENT,RECORD_TIME TEXT NOT NULL,HOST TEXT NOT NULL,LATENCY INT NOT NULL)'
 cu.execute(sql)
 conn.commit()
 conn.close()
--- ping.py
+++ ping.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #_*_coding:utf-8 _*_
-import subprocess,sqlite3,time
+import subprocess,time
 from multiprocessing import Pool
 
 HOSTLIST_PATH = './hostlist'
@@ -17,8 +17,9 @@ def mping(host):
 		rtt = p.stdout.readlines()[-1].strip().split('/')[-3]			#获取3次ping结果的平均值
 		return(host,rtt)
 	else:
-		p.kill()														#运行2秒后如没有结果则直接Kill进程，返回'time out'
-		return(host,"time out")
+		p.kill()
+		rtt=99999														#运行2秒后如没有结果则直接Kill进程，返回'time out'
+		return(host,rtt)
 
 #多进程并发
 def load_proc(hostfile):
@@ -31,39 +32,51 @@ def load_proc(hostfile):
 	p.join()
 	return res
 
-#将数据写入sqlite数据库
-def write_data(rttlist):
-	conn = sqlite3.connect(DB_PATH)
-	cu = conn.cursor()	
-	for rtt in rttlist:
-		sql = "INSERT INTO HOST_LATENCY (HOST,LATENCY) VALUES (\"%s\", \"%s\")" %(rtt[0],rtt[1])
-		cu.execute(sql)
-		conn.commit()
-	conn.close()
+#写入数据，目前支持写入到sqlite数据库和log文件
+class WriteData(object):
+	def __init__(self, rttlist, t):
+		self.rttlist = rttlist
+		self.t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t)) + " "	#格式化时间字符串
 
-#将数据写入log文件
-def write_log(rttlist,t):
-	t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t)) + " "		#格式化时间字符串
-	try:
-		with open(LOG_FILE,'a') as logfile:
-			for rtt in rttlist:				
-				msg = t + rtt[0] + " " + rtt[1] + '\n'
-				logfile.write(msg)
-	except IOError:
-		print('Error: Open logfile failed, please check it...')
-		exit(0)
+#将数据写入到sqlite数据库
+	def to_sqliteDB(self):
+		try:
+			import sqlite3
+		except ImportError:
+			print("Error, Import sqlite3 module failed, please check it...")
+			exit(1)
+		conn = sqlite3.connect(DB_PATH)
+		cu = conn.cursor()	
+		for rtt in self.rttlist:
+			sql = "INSERT INTO HOST_LATENCY (RECORD_TIME,HOST,LATENCY) VALUES (\"%s\", \"%s\", \"%s\")" %(self.t, rtt[0], rtt[1])
+			cu.execute(sql)
+			conn.commit()
+		conn.close()
+
+#将数据写入到log文件
+	def to_logfile(self):			
+		try:
+			with open(LOG_FILE,'a') as logfile:
+				for rtt in self.rttlist:				
+					msg = self.t + rtt[0] + " " + str(rtt[1]) + '\n'
+					logfile.write(msg)
+		except IOError:
+			print("Error: Open logfile failed, please check it...")
+			exit(1)	
 
 def main():
-	start_time = time.time()											#记录程序开始时间
+	start_time = time.time()
 	try:
 		with open(HOSTLIST_PATH,'r') as hostfile:
 			rttlist = load_proc(hostfile)
-			write_log(rttlist,start_time)
+		data = WriteData(rttlist,start_time)
+		data.to_logfile()
+
 	except IOError:
-		print('Error: Open hostfile failed, please check it...')
+		print("Error: Open hostfile failed, please check it...")
 		exit(0)
-	end_time = '%.2f'%(time.time() - start_time)
-	print('Finished in '+ str(end_time) + 's')							#计算程序运行的时间
+	run_time = '%.2f'%(time.time() - start_time)
+	print('Finished in '+ str(run_time) + 's')							#计算程序运行的时间
 
 if __name__ == '__main__':
 	main()
